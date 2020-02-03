07e32875132e0b00685565d82cafa5d646523b1a
Zach Ziino <zachziino@Zachs-MacBook-Pro.local>
2020-02-02 20:09:25 -0800

    changed db email

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
     password= db.Column(db.String(50))
 
 
