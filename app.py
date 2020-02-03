f4e72eb5f60535f45c7067af4e86ea07e5a5178c
Zach Ziino <zachziino@Zachs-MacBook-Pro.local>
2020-02-03 09:42:23 -0800

    email

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
 
-app.config['DEBUG'] = True
-app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'mail.gmx.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'wxsafeflight@gmx.com'
-app.config['MAIL_PASSWORD'] = 'wxsafeflight'
-app.config['MAIL_DEFAULT_SENDER'] = ('Wx Safe Flight', 'wxsafeflight@gmx.com')
-app.config['MAIL_MAX_EMAILS'] = None
-app.config['MAIL_ASCII_ATTACHMENTS'] = False
+# app.config['DEBUG'] = True
+# app.config['TESTING'] = False
+# app.config['MAIL_SERVER'] = 'mail.gmx.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = False
+# app.config['MAIL_USERNAME'] = 'wxsafeflight@gmx.com'
+# app.config['MAIL_PASSWORD'] = 'wxsafeflight'
+# app.config['MAIL_DEFAULT_SENDER'] = ('Wx Safe Flight', 'wxsafeflight@gmx.com')
+# app.config['MAIL_MAX_EMAILS'] = None
+# app.config['MAIL_ASCII_ATTACHMENTS'] = False
 
-mail = Mail(app)
+# mail = Mail(app)
 
 
 
@@ -33,13 +33,13 @@ class User(db.Model):
 
 
 
-@app.route('/mail')
-def send():
-    msg = Message('Welcome!', recipients=['zachziino@pm.me'])
-    msg.html = '<b>Welcome to Wx Safe Flight! Enjoy, and may good weather be with you!</b>'
-    mail.send(msg)
+# @app.route('/mail')
+# def send():
+#     msg = Message('Welcome!', recipients=['zachziino@pm.me'])
+#     msg.html = '<b>Welcome to Wx Safe Flight! Enjoy, and may good weather be with you!</b>'
+#     mail.send(msg)
 
-    return 'Message has been sent!'
+#     return 'Message has been sent!'
 
 
 @app.route('/users')
