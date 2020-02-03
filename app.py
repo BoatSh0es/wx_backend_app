71d4d352a4d988cbdb73928860733bcb311fe151
Zach Ziino <zachziino@Zachs-MacBook-Pro.local>
2020-02-03 10:15:47 -0800

    email

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
+
 db = SQLAlchemy(app)
 
 app.config['DEBUG'] = True
